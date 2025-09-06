5a65b1b28d5504c34093e1d82a416c22392cb775
Owen Petsco <ignotumresponsum@gmail.com>
2025-09-06 12:58:22 -0700

    Added order confirmation page

---
+++ hebrews-coffee-site/src/routes/place-order/[drink]/confirm/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+      import { goto } from '$app/navigation';
+
+      import { storedOrder } from '$lib/stores/storedOrder.js';
+      import { addOrder } from '$lib/stores/orders.js';
+      import { uppercaseComplexString } from '$lib/utils/helpers.js';
+
+      function placeOrder() {
+          // Add the order to the store
+          addOrder($storedOrder);
+
+          // Clear the stored order
+          storedOrder.set(null);
+
+          // Redirect to the order confirmation page
+          goto('/order-confirmation');
+      }
+
+      function editOrder() {
+          // Redirect back to the drink customization page
+          const orderPage = '/place-order/' + $storedOrder.drink.toLowerCase().replace(/ /g, '-');
+          goto(orderPage);
+      }
+</script>
+
+{#if $storedOrder}
+    <h1>Confirm Your Order</h1>
+    <p><strong>Drink:</strong> {uppercaseComplexString($storedOrder.drink)}</p>
+    <p><strong>Temperature:</strong> {$storedOrder.temp}</p>
+    {#if $storedOrder.espresso}
+        <p><strong>Espresso:</strong> {$storedOrder.espresso}</p>
+    {/if}
+    {#if $storedOrder.milk}
+        <p><strong>Milk:</strong> {$storedOrder.milk}</p>
+    {/if}
+    {#if $storedOrder.flavor && $storedOrder.flavor !== 'No Flavor'}
+        <p><strong>Flavor:</strong> {$storedOrder.flavor}</p>
+    {/if}
+
+    <button on:click={placeOrder}>Place Order</button>
+    <button on:click={editOrder}>Edit Order</button>
+{:else}
+    <p>No order to confirm. Please go back and customize your drink.</p>
+{/if}
