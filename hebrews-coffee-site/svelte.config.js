9e8f23e350b2baf8a3f167575309b34e17ce85a2
Owen Petsco <ignotumresponsum@gmail.com>
2025-12-02 15:17:29 -0800

    Updated fallback parameter

--- hebrews-coffee-site/svelte.config.js
+++ hebrews-coffee-site/svelte.config.js
@@ -3,38 +3,35 @@ import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		prerender: {
-			crawl: true,
-			entries: [
-				'/',
-				'/place-order',
-				'/place-order/latte',
-				'/place-order/americano',
-				'/place-order/cold-brew',
-				'/place-order/chai-latte',
-				'/place-order/dirty-chai-latte',
-				'/place-order/london-fog',
-				'/place-order/hot-chocolate',
-				'/place-order/iced-tea',
-				'/place-order/[drink]',
-				'/place-order/[drink]/confirm',
-				'/place-order/[drink]/confirm/order-confirmation'
-			]
-		},
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			pages: path.resolve('../build'),
-			assets: path.resolve('../build'),
-			fallback: null
-		}),
-		paths: {
-			base: '/hebrews-coffee-site',
-			relative: false
-		}
-	}
+        kit: {
+                prerender: {
+                        crawl: true,
+                        entries: [
+                                '/',
+                                '/place-order',
+                                '/place-order/latte',
+                                '/place-order/americano',
+                                '/place-order/cold-brew',
+                                '/place-order/chai-latte',
+                                '/place-order/dirty-chai-latte',
+                                '/place-order/london-fog',
+                                '/place-order/hot-chocolate',
+                                '/place-order/iced-tea',
+                                '/place-order/[drink]',
+                                '/place-order/[drink]/confirm',
+                                '/place-order/[drink]/confirm/order-confirmation'
+                        ]
+                },
+                adapter: adapter({
+                        pages: path.resolve('../build'),
+                        assets: path.resolve('../build'),
+                        fallback: '404.html'  // CHANGED: was null, now '404.html'
+                }),
+                paths: {
+                        base: '/hebrews-coffee-site',
+                        relative: false
+                }
+        }
 };
 
 export default config;
